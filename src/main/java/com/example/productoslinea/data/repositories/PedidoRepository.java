2d1d895772083277ec56ebd2c813d5eeb4de5e1b
JuanP10 <ramirezgutierrez32@gmail.com>
2024-04-02 18:56:42 -0500

    Detalles

--- src/main/java/com/example/productoslinea/data/repositories/PedidoRepository.java
+++ src/main/java/com/example/productoslinea/data/repositories/PedidoRepository.java
@@ -1,21 +1,2 @@
-package com.example.productoslinea.data.repositories;
-
-import com.example.productoslinea.data.entities.Cliente;
-import com.example.productoslinea.data.entities.Enums.Estado;
-import com.example.productoslinea.data.entities.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-
-import java.time.LocalDate;
-import java.util.List;
-@Repository
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-
-    List<Pedido> findByFechaPedidoBetween(LocalDate fechaInicio, LocalDate fechaFin);
-    List<Pedido> findByClienteAndEstado(Cliente cliente, Estado estado);
-
-    @Query("SELECT DISTINCT p FROM Pedido p LEFT JOIN FETCH p.items WHERE p.cliente = ?1")
-    List<Pedido> recuperarPedidosConArticulosPorCliente(@Param( "cliente") Long idCliente);
+package com.example.productoslinea.data.repositories;public interface PedidoRepository {
 }
